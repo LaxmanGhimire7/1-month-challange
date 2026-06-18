5c97d23c29768193a3d501c15368dab604e56b9c
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-06-18 23:03:25 +0545

    programmed server to give user response

--- basic start/basci-1/server.js
+++ basic start/basci-1/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express() //server ko instance create garne
 
+app.get("/",(req,res)=>{
+    res.send("Hello AI Engineer")
+})
 
+app.get("/lakxh",(req,res)=>{
+    res.send("This is me Lakxh and I am going to be a strong AI Engineer...")
+})
 
 app.listen(3000)
