d74728ebdd6b21f667fb39bff0ba513faf8e3003
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-06-19 22:38:14 +0545

    connected to DB and API creation 19

--- basic start/basic-4_DB/src/app.js
+++ basic start/basic-4_DB/src/app.js
@@ -9,22 +9,33 @@ app.use(express.json());
 /**
  * POST -> {userName, age} = req.body;
  */
-app.post("/api/user", async(req, res) => {
+app.post("/api/user", async (req, res) => {
   const { userName, age } = req.body;
   const users = await userModel.create({ userName, age });
   res.status(201).json({
     message: "User created",
-    users
+    users,
   });
 });
 
 // GET ->
-app.get("/api/user",async(req,res)=>{
-    const users = await userModel.find();
-    res.status(200).json({
-        message:"Users fetched",
-        users
-    })
-})
+app.get("/api/user", async (req, res) => {
+  const users = await userModel.find();
+  res.status(200).json({
+    message: "Users fetched",
+    users,
+  });
+});
+
+/**
+ * DELETE
+ */
+app.delete("/api/user/:id", async (req, res) => {
+  const id = req.params.id;
+  await userModel.findByIdAndDelete(id);
+  res.status(200).json({
+    message: "User deleted successfully",
+  });
+});
 
 module.exports = app;
