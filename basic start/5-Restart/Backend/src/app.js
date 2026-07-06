7a5d964875524a496d9f8f4a6abd0ebacd03b28b
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-06 17:55:14 +0545

    crud

--- basic start/5-Restart/Backend/src/app.js
+++ basic start/5-Restart/Backend/src/app.js
@@ -18,6 +18,35 @@ app.post("/api/register", async(req, res) => {
   })
 })
 
+//GET
+app.get("/api/user",async(req,res)=>{
+   const user = await userModel.find();
+   res.status(200).json({
+    message: "User fetched successfully",
+    user
+   })
+})
+
+
+//Delete -> req.parama.id
+app.delete("/api/delete/:id",async(req,res)=>{
+    const id = req.params.id;
+    console.log(id)
+    await userModel.findByIdAndDelete(id)
+    res.status(200).json({
+        message: "User deleted successfully"
+    })
+})
+
+//update -> req.params.id = req.body;
+app.patch("/api/update/:id",async(req,res)=>{
+    const id = req.params.id;
+    const {age,country  } = req.body;
+    await userModel.findByIdAndUpdate(id,{age,country})
+    res.status(200).json({
+        message:"User Updated Successfully...",
+    })
+})
 
 
 module.exports = app;
