34d3424ec0211833b231a39f193f75dd778dac5f
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-13 23:24:00 +0545

    another

--- basic start/auth-13date/Backend/src/model/user.model.js
+++ basic start/auth-13date/Backend/src/model/user.model.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String
-    },
-    email:{
-        unique:true,
-        type:String
-    },
-    password:{
-        type:String
-    }
-})
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: [true, "With this email user already exists"],
+  },
+  password: {
+    type: String,
+  },
+});
 
-const userModel = mongoose.model("users",userSchema);
+const userModel = mongoose.model("users", userSchema);
 module.exports = userModel;
--- basic start/auth-13date/Backend/src/routes/auth.routes.js
+++ basic start/auth-13date/Backend/src/routes/auth.routes.js
@@ -3,11 +3,18 @@ const userModel = require("../model/user.model");
 
 const authRouter = express.Router();
 
-authRouter.post("/register", (req, res) => {
+authRouter.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
-  const user = userModel.create({ username, email, password });
+  const isUserExists = await userModel.findOne({ email });
+  if (isUserExists) {
+   return res.status(409).json({
+      message: "User already exists with this email, try with new one",
+    });
+  }
+  const user = await userModel.create({ username, email, password });
   res.status(201).json({
     message: "User registerd successfully...",
+    user,
   });
 });
 
