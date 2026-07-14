6b2500c900e09d3633931efb511baf2d29a89a0c
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-14 20:45:18 +0545

    login api

--- basic start/14-July-JWT/src/Routes/auth.route.js
+++ basic start/14-July-JWT/src/Routes/auth.route.js
@@ -22,9 +22,41 @@ authRouter.post("/register", async (req, res) => {
     process.env.JWT_SECRETS,
   );
 
-  res.cookie("jwt_token", token)
+  res.cookie("jwt_token", token);
   res.status(201).json({
     message: "User registered successsfully...",
+    user,
+    token,
+  });
+});
+
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found!",
+    });
+  }
+
+  const passwordMatched = user.password === password;
+  if (!passwordMatched) {
+    return res.status(401).json({
+      message: "Invalid Password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRETS,
+  );
+  res.cookie("jwt_token", token);
+  res.status(200).json({
+    message: "User login successful",
     user,
     token,
   });
