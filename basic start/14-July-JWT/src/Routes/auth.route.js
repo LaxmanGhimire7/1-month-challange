9202cf205d3ff71ff7994c73beddfd0ee4492dda
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-14 19:40:50 +0545

    added cookie

--- basic start/14-July-JWT/src/Routes/auth.route.js
+++ basic start/14-July-JWT/src/Routes/auth.route.js
@@ -1,35 +1,33 @@
 const express = require("express");
-const userModel = require("../Models/user.model")
-const jwt = require("jsonwebtoken")
+const userModel = require("../Models/user.model");
+const jwt = require("jsonwebtoken");
 
 const authRouter = express.Router();
 
-authRouter.post("/register",async(req,res)=>{
-const {username, email, password} = req.body;
+authRouter.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
 
-const userAlreadyExist = await userModel.findOne({email});
-if(userAlreadyExist){
-    return res.status(409).json({message:"User already exists"})
-}
+  const userAlreadyExist = await userModel.findOne({ email });
+  if (userAlreadyExist) {
+    return res.status(409).json({ message: "User already exists" });
+  }
 
-const user = await userModel.create({username, email, password});
+  const user = await userModel.create({ username, email, password });
 
-const token = jwt.sign({
-    id:user._id,
-    email:user.email
-},
-process.env.JWT_SECRETS
-)
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRETS,
+  );
 
-res.status(201).json({
-    message:"User registered successsfully...",
+  res.cookie("jwt_token", token)
+  res.status(201).json({
+    message: "User registered successsfully...",
     user,
-    token
-
-})
-
-})
-
-
+    token,
+  });
+});
 
 module.exports = authRouter;
--- basic start/14-July-JWT/src/app.js
+++ basic start/14-July-JWT/src/app.js
@@ -1,12 +1,13 @@
 // server config and start garna
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser")
 const authRouter = require("./Routes/auth.route")
 
 
 // middleware
 app.use(express.json());
-
+app.use(cookieParser())
 
 //api
 app.use("/api/auth", authRouter)
