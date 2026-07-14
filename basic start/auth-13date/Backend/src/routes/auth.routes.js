656ee4f1a6adb20a83be279b2a1696af790c8ee9
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-14 18:53:06 +0545

    added cookie, token

--- basic start/auth-13date/Backend/src/routes/auth.routes.js
+++ basic start/auth-13date/Backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userModel = require("../model/user.model");
+const jwt = require("jsonwebtoken");
 
 const authRouter = express.Router();
 
@@ -7,14 +8,25 @@ authRouter.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
   const isUserExists = await userModel.findOne({ email });
   if (isUserExists) {
-   return res.status(409).json({
+    return res.status(409).json({
       message: "User already exists with this email, try with new one",
     });
   }
   const user = await userModel.create({ username, email, password });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
   res.status(201).json({
     message: "User registerd successfully...",
     user,
+    token,
   });
 });
 
