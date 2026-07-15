7c2f15c49a4802138ba4bfbfe9371103115b1961
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-15 17:07:47 +0545

    login: either email or username and pass

--- basic start/INSTACLONE/Backend/src/routes/auth.route.js
+++ basic start/INSTACLONE/Backend/src/routes/auth.route.js
@@ -47,15 +47,69 @@ authRouter.post("/register", async (req, res) => {
   res.cookie("token", token);
   res.status(201).json({
     message: "User created successfully",
-    user:{
-        username:user.username,
-        email:user.email,
-        bio:user.bio,
-        profileImage:user.profileImage
-
+    user: {
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profileImage: user.profileImage,
     },
     token,
   });
+});
+
+//Login
+authRouter.post("/login", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const user = await userModel.findOne({
+    $or: [
+      {
+        /*
+        conditions: if username bata login garye yo linxa email undefined aauxa so yo matra chalxa 
+        */
+        username: username,
+      },
+      {
+        /*
+        conditions: if email provide garyo vanye yo chalxa username wala condition undefined==false soo tyo chaldaina
+        */
+        email: email,
+      },
+    ],
+  });
+
+  if(!user){
+    return res.status(404).json({
+        message: "User not found"
+    })
+  }
+
+  const hash = crypto.createHash("md5").update(password).digest("hex");
+
+  const isPasswordValid = hash == user.password;
+  if(!isPasswordValid){
+    return res.status(401).json({
+        message: "Invalid password"
+    })
+  }
+
+  const token = jwt.sign({
+    id:user._id,
+    email:user.email,
+  },
+process.env.JWT_SECRET,{expiresIn:"1d"})
+
+res.cookie("token", token)
+res.status(200).json({
+    message: "User login successful",
+    user:{
+        username: user.username,
+        email: user.email,
+        bio:user.bio,
+        profileImage:user.profileImage
+    },
+    token
+})
 });
 
 module.exports = authRouter;
