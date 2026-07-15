c4bbcc671e6a602487c5878687012e95edec2b12
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-15 08:20:09 +0545

    practice done

--- basic start/practice-15-auth/Backend/src/routes/auth.routes.js
+++ basic start/practice-15-auth/Backend/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userModel = require("../model/user.model");
 const crypto = require("crypto");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const Router = express.Router();
 
@@ -22,20 +22,68 @@ Router.post("/register", async (req, res) => {
     password: crypto.createHash("md5").update(password).digest("hex"),
   });
 
-  const token = jwt.sign({
-    id:user._id,
-    email:user.email
-  },
-  process.env,JWT_SECRET
-)
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
 
-res.cookie("token", token)
-res.status(201).json({
-    message:"User registered successfully...",
+  res.cookie("token", token);
+  res.status(201).json({
+    message: "User registered successfully...",
     user,
-    token
+    token,
+  });
+});
+
+//Authentication
+Router.get("/get-me",async(req,res)=>{
+    const token = req.cookies.token
+    console.log(token)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(token)
+    const user = await userModel.findById(decoded.id)
+    console.log(user)
+    res.json({
+        user
+    })
 })
 
+//Login
+Router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(409).json({
+      message: "User donot exists",
+    });
+  }
+
+  const matchedPassword =
+    user.password === crypto.createHash("md5").update(password).digest("hex");
+  if (!matchedPassword) {
+    return res.status(400).json({
+      message: "Invalid Password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("token", token);
+  res.status(200).json({
+    message: "User login successfully",
+    user,
+    token,
+  });
 });
 
 module.exports = Router;
