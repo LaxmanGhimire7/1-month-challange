db928ae794f1788f00a568ee116c24dc9f23064f
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-15 15:14:27 +0545

    models/schema created

---
+++ basic start/INSTACLONE/Backend/src/model/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: [true, "username already exists"],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "email already exists"],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  bio:{
+    type:String
+  },
+  profileImage: {
+    type: String,
+    default:
+      "https://ik.imagekit.io/3wmfdkip4/istockphoto-1451587807-612x612.jpg",
+  },
+});
+
+module.exports = userSchema = mongoose.model("users", userSchema);
