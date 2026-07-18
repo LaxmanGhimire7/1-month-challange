4e0e9d2e8d3cfded3bc64973ba2882cf76a52c36
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-18 23:39:03 +0545

    post created successfully

--- basic start/instaPracticeAgain/Backend/src/controller/post.controller.js
+++ basic start/instaPracticeAgain/Backend/src/controller/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../models/post.model");
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken")
 
 const client = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
@@ -9,6 +10,26 @@ const client = new ImageKit({
 
 const createPostController = async (req, res) => {
 //   console.log(req.body, req.file);
+const token = req.cookies.token
+// console.log(token)
+
+if(!token){
+    return res.status(404).json({
+        messafe: "Unautnorized access"
+    })
+}
+
+
+let decoded = null;
+try {
+decoded = jwt.verify(token, process.env.JWT_SECRET)
+} catch (error) {
+    return res.status(401).json({
+        message:"User not authorized"
+    })
+}
+
+// console.log(decoded)
 
   const response = await client.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
@@ -16,6 +37,16 @@ const createPostController = async (req, res) => {
     folder:"practice"
   });
 //   console.log(response)
+const post = await postModel.create({
+    caption: req.body.caption,
+    imageUrl: response.url,
+    users: decoded.id
+})
+
+  res.status(201).json({
+        message:"Post created successfully",
+        post
+    })
 };
 
 module.exports = { createPostController };
