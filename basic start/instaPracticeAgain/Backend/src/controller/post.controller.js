67400256098223f2a25c09a9584098ee6ee06b90
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-18 22:53:11 +0545

    done upload in imagekit

--- basic start/instaPracticeAgain/Backend/src/controller/post.controller.js
+++ basic start/instaPracticeAgain/Backend/src/controller/post.controller.js
@@ -1,11 +1,21 @@
 const postModel = require("../models/post.model");
+const ImageKit = require("@imagekit/nodejs");
+const { toFile } = require("@imagekit/nodejs");
+
+const client = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
+});
 
 
+const createPostController = async (req, res) => {
+//   console.log(req.body, req.file);
 
+  const response = await client.files.upload({
+    file: await toFile(Buffer.from(req.file.buffer), "file"),
+    fileName: "lakxh",
+    folder:"practice"
+  });
+//   console.log(response)
+};
 
-const createPostController = async(req,res)=>{
-
-
-}
-
-module.exports = {createPostController}
+module.exports = { createPostController };
--- basic start/instaPracticeAgain/Backend/src/routes/post.route.js
+++ basic start/instaPracticeAgain/Backend/src/routes/post.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const {createPostController} = require("../controller/post.controller");
+const multer = require("multer");
+const uploads = multer({storage:multer.memoryStorage()})
 
 const postRouter = express.Router();
 
 
-
+postRouter.post("/",uploads.single("image"), createPostController)
 
 module.exports = postRouter;
