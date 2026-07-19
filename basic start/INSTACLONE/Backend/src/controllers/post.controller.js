e2b5caa018fbf9abdf39cb3faa7a37476b69e2d5
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-19 19:15:09 +0545

    api created to fetch all the posts from user

--- basic start/INSTACLONE/Backend/src/controllers/post.controller.js
+++ basic start/INSTACLONE/Backend/src/controllers/post.controller.js
@@ -12,7 +12,7 @@ const createPostController = async (req, res) => {
   //   console.log(req.body, req.file);
 
   const token = req.cookies.token; // to extract token
-//   console.log(token);
+  //   console.log(token);
 
   if (!token) {
     return res.status(401).json({
@@ -31,23 +31,49 @@ const createPostController = async (req, res) => {
 
   console.log(decoded);
 
-    const response = await client.files.upload({
-      file: await toFile(Buffer.from(req.file.buffer), "file"),
-      fileName: "hii",
-      folder:"cohort-2-instaclone"
-    });
+  const response = await client.files.upload({
+    file: await toFile(Buffer.from(req.file.buffer), "file"),
+    fileName: "hii",
+    folder: "cohort-2-instaclone",
+  });
 
-    const post = await postModel.create({
-        caption: req.body.caption,
-        imageUrl:response.url,
-        users:decoded.id
-    })
-    res.status(201).json({
-        message:"Post created successfully",
-        post
-    })
+  const post = await postModel.create({
+    caption: req.body.caption,
+    imageUrl: response.url, // this because in response the data is there url: so just taking that part ans storing it in imageUrl
+    users: decoded.id,
+  });
+  res.status(201).json({
+    message: "Post created successfully",
+    post,
+  });
 };
+
+// get -> all data created by user
+const getPostController = async (req, res) => {
+  const token = req.cookies.token;
+
+  let decodedData = null;
+  try {
+    decodedData = await jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+  // console.log(decodedData)
+  const userId = decodedData.id;
+
+const posts = await postModel.find({users:userId})
+// console.log(posts)
+res.status(200).json({
+  message:"Post fetched successfully...",
+  posts
+})
+};
+
+
 
 module.exports = {
   createPostController,
+  getPostController,
 };
