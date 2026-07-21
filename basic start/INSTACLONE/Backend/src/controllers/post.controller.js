60182121fd09951dca418e0a7f0c2622e490d2f4
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-07-21 18:52:15 +0545

    created middleware

--- basic start/INSTACLONE/Backend/src/controllers/post.controller.js
+++ basic start/INSTACLONE/Backend/src/controllers/post.controller.js
@@ -12,26 +12,6 @@ const client = new ImageKit({
 const createPostController = async (req, res) => {
   //   console.log(req.body, req.file);
 
-  const token = req.cookies.token; // to extract token
-  //   console.log(token);
-
-  if (!token) {
-    return res.status(401).json({
-      message: "Token not provided, Unauthorized access",
-    });
-  }
-
-  let decoded = null;
-  try {
-    decoded = jwt.verify(token, process.env.JWT_SECRET);
-  } catch (error) {
-    return res.status(401).json({
-      message: "User not authorized",
-    });
-  }
-
-  console.log(decoded);
-
   const response = await client.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "hii",
@@ -41,7 +21,7 @@ const createPostController = async (req, res) => {
   const post = await postModel.create({
     caption: req.body.caption,
     imageUrl: response.url, // this because in response the data is there url: so just taking that part ans storing it in imageUrl
-    users: decoded.id,
+    users: req.user.id,
   });
   res.status(201).json({
     message: "Post created successfully",
@@ -51,78 +31,76 @@ const createPostController = async (req, res) => {
 
 // get -> all data created by user
 const getPostController = async (req, res) => {
-  const token = req.cookies.token;
+  // const token = req.cookies.token;
 
-  let decodedData = null;
-  try {
-    decodedData = await jwt.verify(token, process.env.JWT_SECRET);
-  } catch (error) {
-    res.status(401).json({
-      message: "Invalid token",
-    });
-  }
-  // console.log(decodedData)
-  const userId = decodedData.id;
+  // let decodedData = null;
+  // try {
+  //   decodedData = await jwt.verify(token, process.env.JWT_SECRET);
+  // } catch (error) {
+  //   res.status(401).json({
+  //     message: "Invalid token",
+  //   });
+  // }
+  //  console.log(decodedData)
 
-const posts = await postModel.find({users:userId})
-// console.log(posts)
-res.status(200).json({
-  message:"Post fetched successfully...",
-  posts
-})
+  const userId = req.user.id;
+  console.log(userId)
+
+  const posts = await postModel.find({ users: userId });
+  // console.log(posts)
+  res.status(200).json({
+    message: "Post fetched successfully...",
+    posts,
+  });
 };
 
+const getPostDetailsController = async (req, res) => {
+  //  const token = req.cookies.token;
+  //  if(!token){
+  //   return res.status(401).json({
+  //     message:"Unauthorized access"
+  //   })
+  //  }
 
-const getPostDetailsController = async(req,res)=>{
-   const token = req.cookies.token;
-   if(!token){
-    return res.status(401).json({
-      message:"Unauthorized access"
-    })
-   } 
+  // let decodedData;
+  // try {
+  //     decodedData = await jwt.verify(token, process.env.JWT_SECRET)
+  // } catch (error) {
+  //   res.status(401).json({
+  //     message: "Invalid token"
+  //   })
+  // }
 
-  let decodedData;
-  try {
-      decodedData = await jwt.verify(token, process.env.JWT_SECRET)
-  } catch (error) {
-    res.status(401).json({
-      message: "Invalid token"
-    })
-  }
+  // console.log(decodedData)
 
-  console.log(decodedData)
-
-  const userId = decodedData.id;
-  const postId = req.params.postId
+  const userId = req.user.id;
+  const postId = req.params.postId;
   //  console.log(userId)
   // console.log(postId)
-  
+
   const posts = await postModel.findById(postId);
   // console.log(posts)
-  if(!posts){
+  if (!posts) {
     return res.status(404).json({
-      message: "Post not found"
-    })
+      message: "Post not found",
+    });
   }
 
   const isValidUser = posts.users.toString() === userId;
-if(!isValidUser){
-  return res.status(403).json({
-    message:"Forbidden content"
-  })
-}
+  if (!isValidUser) {
+    return res.status(403).json({
+      message: "Forbidden content",
+    });
+  }
 
-return res.status(200).json({
-  message:"Post fetched syccessfully",
-  posts
-})
-
-
-}
-
+  return res.status(200).json({
+    message: "Post fetched syccessfully",
+    posts,
+  });
+};
 
 module.exports = {
   createPostController,
   getPostController,
-  getPostDetailsController
+  getPostDetailsController,
 };
--- basic start/INSTACLONE/Backend/src/routes/post.route.js
+++ basic start/INSTACLONE/Backend/src/routes/post.route.js
@@ -6,11 +6,12 @@ const {
 } = require("../controllers/post.controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
+const identifyUser = require("../middlewares/auth.middleware")
 
 const postRouter = express.Router();
 
-postRouter.post("/", upload.single("image"), createPostController);
-postRouter.get("/", getPostController);
-postRouter.get("/details/:postId", getPostDetailsController);
+postRouter.post("/", upload.single("image"),identifyUser, createPostController);
+postRouter.get("/",identifyUser, getPostController);
+postRouter.get("/details/:postId",identifyUser, getPostDetailsController);
 
 module.exports = postRouter;
