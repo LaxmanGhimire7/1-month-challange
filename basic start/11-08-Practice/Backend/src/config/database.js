b864147e8bbd161438e22ac7834a48db4345cd95
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-08-11 08:34:38 +0545

    done

--- basic start/11-08-Practice/Backend/src/config/database.js
+++ basic start/11-08-Practice/Backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectToDb = () => {
-  mongoose
+const connectToDb = async () => {
+  await mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
       console.log("Connected to Db...");
---
+++ basic start/11-08-Practice/Backend/src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        required:[true, "username is required"],
+        unique:[true,"username already exists"]
+    },
+    email:{
+        type:String,
+        required:[true, "email is required"],
+        unique:[true,"email already exists"]
+    },
+    profileImage:{
+        type:String,
+        default:"https://ik.imagekit.io/3wmfdkip4/lakxh_tS-LQOo4_?updatedAt=1784141737902"
+    },
+    password:{
+        type:String,
+        required:[true, "password is required"]
+    },
+    bio:{
+        type:String
+    }
+})
+
+module.exports = userModel = await mongoose.model("users",userSchema)
