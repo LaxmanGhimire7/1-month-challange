f9feaadc298f3d7213ab3c75dfb8f25943b06bfc
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-09-06 20:56:22 +0545

    postdetail

--- basic start/september-5/Backend/src/controllers/post.controller.js
+++ basic start/september-5/Backend/src/controllers/post.controller.js
@@ -9,8 +9,6 @@ const client = new ImageKit({
 
 const createPostController = async (req, res) => {
   //   console.log(req.cookies)
- 
-
   const response = await client.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "fileName",
@@ -30,14 +28,40 @@ const createPostController = async (req, res) => {
 
 const getPostController = async (req, res) => {
   const userId = req.user.id;
-//   console.log(userId)
-  const post = await postModel.find({users:userId})
+  //   console.log(userId)
+  const post = await postModel.find({ users: userId });
   res.status(200).json({
-    message:"Post fetched successfully",
-    post
-  })
+    message: "Post fetched successfully",
+    post,
+  });
 };
 
+const getPostDetailController = async (req, res) => {
+  const userId = req.user.id;
+  //   console.log(userId)
+  const postId = req.params.postId;
+  // console.log(postId)
+  const post = await postModel.findById(postId);
+  console.log(post);
+  if (!post) {
+    return res.status(404).json({
+      message: "Post not found",
+    });
+  }
+  const isValidUser = post.users.toString() === userId;
+  if (!isValidUser) {
+    return res.status(403).json({
+      message: "Forbidden content",
+    });
+  }
+  return res.status(200).json({
+    message: "Post deatails fetched successfully",
+    post,
+  });
+};
 
-
-module.exports = { createPostController, getPostController };
+module.exports = {
+  createPostController,
+  getPostController,
+  getPostDetailController,
+};
--- basic start/september-5/Backend/src/routes/post.routes.js
+++ basic start/september-5/Backend/src/routes/post.routes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {createPostController, getPostController} = require("../controllers/post.controller")
+const {createPostController, getPostController,getPostDetailController} = require("../controllers/post.controller")
 const identifyUser = require("../middlewares/auth.middleware")
 const multer = require("multer")
 const upload = multer({storage:multer.memoryStorage()})
@@ -8,5 +8,6 @@ const postRouter = express.Router()
 
 postRouter.post("/",upload.single("image"), identifyUser, createPostController)
 postRouter.get("/", identifyUser, getPostController)
+postRouter.get("/details/:postId", identifyUser, getPostDetailController)
 
 module.exports = postRouter;
