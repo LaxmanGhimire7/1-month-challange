db046d69ce71f7b5e2774f62fc64cf1555fa2ffc
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-09-20 17:49:16 +0545

    database connected

---
+++ basic start/sept20/Backend/src/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectToDb=async (req,res)=>{
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("Connected to Db...")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
+module.exports = connectToDb;
